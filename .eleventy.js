9949a2280e9624812fed804966ae79e4869d830e
Timo Eemeli Koola <timo@timokoola.com>
2022-02-07 20:28:44 +0200

    more stuff

--- .eleventy.js
+++ .eleventy.js
@@ -41,7 +41,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static/img");
 
   // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/favicon*");
 
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
